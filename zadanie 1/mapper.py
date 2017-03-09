d186c0b87381b70f8b5f3614e10380ab58385a6b
Paweł Marcin Chojnacki <pawel.marcin.chojnacki@gmail.com>
2017-03-09 20:05:25 +0100

    Added ignoring stopwords (case insensitive)

--- zadanie 1/mapper.py
+++ zadanie 1/mapper.py
@@ -1,7 +1,12 @@
 import sys
 
+stopwords = set(stopword.strip() for stopword in open('stopwords'))
 for line in sys.stdin:
 	stripped_line = line.strip()
 	words = stripped_line.split()
 	for word in words:
-		print(word + '\t1')
+		word = word.lower()
+		if word in stopwords:
+			continue
+		else:
+			print(word + '\t1')
