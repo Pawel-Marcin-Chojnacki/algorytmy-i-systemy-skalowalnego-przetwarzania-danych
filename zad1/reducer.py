ff31a91551afbe1aae891bb38990673ad5bf7ed1
Paweł Marcin Chojnacki <pawel.marcin.chojnacki@gmail.com>
2017-03-19 00:56:59 +0100

    PyCharm autorefactor.

--- zad1/reducer.py
+++ zad1/reducer.py
@@ -6,14 +6,14 @@ word_counter = 1
 key = ''
 value = 0
 for line in sys.stdin:
-	key, value = line.strip().split("\t")
-	if key == pocket:
-		word_counter = word_counter + int(value)
-	else:
-		print(key + '\t', word_counter)
-		pocket = key
-		word_counter = int(value)
+    key, value = line.strip().split("\t")
+    if key == pocket:
+        word_counter = word_counter + int(value)
+    else:
+        print(key + '\t', word_counter)
+        pocket = key
+        word_counter = int(value)
 if word_counter > 1:
-	print(key + '\t', word_counter)
-	pocket = key
-	word_counter = int(value)
+    print(key + '\t', word_counter)
+    pocket = key
+    word_counter = int(value)
